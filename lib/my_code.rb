07546a1cab876d960fcd4e91b94392035258cbc6
Michael Newman <coloradohokie@gmail.com>
2019-12-20 06:07:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,6 +11,25 @@ end #method
 
 
 def reduce(array, starting_point=nil)
+  new_array = array
+  if starting_point
+    new_array.shift(starting_point)
+  end
+    
+  total = new_array[0]
+  i=1
+  
+  
+  while i < new_array.length do
+    total = yield(total, new_array[i])
+    i += 1
+  end #while
+  return total
+end #method
+
+
+=begin    
+def reduce(array, starting_point=nil)
   if starting_point
     total = starting_point
     i=0
@@ -26,6 +45,5 @@ def reduce(array, starting_point=nil)
   end #while
   return total
 end #method
-    
-
+=end
 
